1beca6d522c4b6343ba0460ae974d3e425cbf20f
gunjan.arora <gunjan.arora@fairfaxmedia.com.au>
2019-02-04 09:42:50 +1100

    Cleanup the code

--- testing/weatherFiveDays.js
+++ testing/weatherFiveDays.js
@@ -3,16 +3,16 @@ const request = require('supertest')('api.openweathermap.org/data/2.5/forecast')
 require('dotenv').config()
 
 function getRequestForFiveDays() {
-    try {
-      let data
-      const cityName ='Sydney'
-      const appid = process.env.API_KEY
-      return data = request.get(`?q=${cityName}&appid=${appid}`)
-    } catch (error) {
-      throw Error(error)
-    }
+  try {
+    let data
+    const cityName ='Sydney'
+    const appid = process.env.API_KEY
+    return data = request.get(`?q=${cityName}&appid=${appid}`)
+  } catch (error) {
+    throw Error(error)
   }
+}
 
-  module.exports = {
-    getRequestForFiveDays,
-  }
+module.exports = {
+  getRequestForFiveDays,
+}
